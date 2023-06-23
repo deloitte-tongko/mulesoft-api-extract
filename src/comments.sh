5bb2e7fb5c3ff5a966df5ebb85111d61dc067781
Timothy Ongko <tong0006@student.monash.edu>
2023-06-23 17:18:16 +1000

    Fixed a major bug with newlines popping up from comments

--- src/comments.sh
+++ src/comments.sh
@@ -1,6 +1,6 @@
 COMMENTS=""
 if [[ -d "$BASE_PATH/src/main/app" && -f $BASE_PATH/src/main/app/config.xml ]]; then
-	COMMENT=$(cat $BASE_PATH/src/main/app/config.xml | egrep -Eo "<([[:alpha:]]|:|-)*\s" | egrep -Eo "[^<].*" | egrep -Ev "spring:" | sort --unique | xargs | sed 's/ /\//g');
+	COMMENT=$(cat $BASE_PATH/src/main/app/config.xml | egrep -Eo "<([[:alpha:]]|:|-)*\s" | egrep -Eo "[^<].*" | egrep -Ev "spring:" | sort --unique | xargs | sed 's/ /\//g' | tr -d '\r');
 fi
 
 HEADING="$HEADING,comments";
