58230751743357757214e4cab28b1d50d20ffedc
Timothy Ongko <tong0006@student.monash.edu>
2023-06-23 16:51:34 +1000

    Added some checks to prevent errors

--- src/comments.sh
+++ src/comments.sh
@@ -1,6 +1,5 @@
 COMMENTS=""
-
-if [[ -d "$BASE_PATH/src/main/app" ]]; then
+if [[ -d "$BASE_PATH/src/main/app" && -f $BASE_PATH/src/main/app/config.xml ]]; then
 	COMMENT=$(cat $BASE_PATH/src/main/app/config.xml | egrep -Eo "<([[:alpha:]]|:|-)*\s" | egrep -Eo "[^<].*" | egrep -Ev "spring:" | sort --unique | xargs | sed 's/ /\//g');
 fi
 
